9237558cad4cd546ba975e49825fb89212f9a689
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-03 16:58:00 -0300

    add image pipe and get one hero by sending id

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
@@ -14,6 +16,8 @@ import { MatCardModule } from '@angular/material/card';
     MatIconModule,
     MatListModule,
     MatCardModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
   ],
 })
 export class MaterialModule {}
